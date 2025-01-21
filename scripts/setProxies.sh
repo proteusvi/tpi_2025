ff3d755349694725cb66bf5a6ed5fa8be2beb6a6
Beux Didier (DSE) <didier.beux@etat.ge.ch>
2025-01-21 12:23:09 +0100

    PHP-914 - Remove scripts from git.

--- scripts/setProxies.sh
+++
@@ -1,6 +0,0 @@
-set ftp_proxy=http://192.168.127.254:3128
-set https_proxy=http://192.168.127.254:3128
-set FTP_PROXY=http://192.168.127.254:3128
-set HTTPS_PROXY=http://192.168.127.254:3128
-set HTTP_PROXY=http://192.168.127.254:3128
-set http_proxy=http://192.168.127.254:3128
--- scripts/unsetProxies.sh
+++
@@ -1,6 +0,0 @@
-unset ftp_proxy
-unset https_proxy
-unset FTP_PROXY
-unset HTTPS_PROXY
-unset HTTP_PROXY
-unset http_proxy
